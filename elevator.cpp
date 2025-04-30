3b21633674727d1df0c7efc7c71d996643724b92
KrishPatel1605 <krishpatel160512@gmail.com>
2025-04-30 16:54:57 +0530

    getting done every thing on classes

--- elevator.cpp
+++ elevator.cpp
@@ -5,36 +5,65 @@
 
 using namespace std;
 
+class FetchFloorData
+{
+private:
+    vector<vector<int>> floordata;
+
+public:
+    FetchFloorData()
+    {
+        floordata.resize(7);
+    }
+
+    void read(string filename)
+    {
+        ifstream fin(filename);
+
+        string line;
+
+        getline(fin, line); // skips the header in csv file
+
+        while (getline(fin, line))
+        {
+            stringstream s(line);
+            vector<string> row;
+            string word;
+
+            while (getline(s, word, ','))
+            {
+                row.push_back(word);
+            }
+
+            int day = stoi(row[1]);
+            int floor = stoi(row[3]);
+
+            floordata[day - 1].push_back(floor);
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < floordata[i].size(); j++)
+            {
+                cout << floordata[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    vector<vector<int>> getData()
+    {
+        return floordata;
+    }
+};
+
 int main()
 {
-    fstream fin("data.csv");
-    vector<vector<int>> floordata(7);
-    string line;
-
-    getline(fin, line); //skips the header in csv file
-
-    while (getline(fin, line))
-    {
-        stringstream s(line);
-        vector<string> row;
-        string word;
-        while (getline(s, word, ','))
-        {
-            row.push_back(word);
-        }
-        int day = stoi(row[1]);
-        int floor = stoi(row[3]);
-        floordata[day - 1].push_back(floor);
-    }
-
-    for (int i = 0; i < 7; i++)
-    {
-        for (int j = 0; j < floordata[i].size(); j++)
-        {
-            cout << floordata[i][j] << " ";
-        }
-        cout << endl;
-    }
-
+    FetchFloorData data;
+    data.read("data.csv");
+    data.print();
     return 0;
 }
